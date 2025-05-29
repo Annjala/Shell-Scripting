ce48019d54c6f4d69f60aabd5035ea91b56446df
Annjala Shaji <118911811+Annjala@users.noreply.github.com>
2025-05-29 14:03:08 +0530

    Update displayfiles.sh

--- displayfiles.sh
+++ displayfiles.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#Display list of files in a directory
 
+#!/bin/bash
 echo 'To display list of files, enter the directory name :'
 read dir
 if [ ! -d "$HOME/$dir" ];then
-	echo "Directory doesn't exist"
+	echo "Directory doesn't exist!"
 else
 	cd $HOME/$dir
-		echo 'List of files :'
+		echo 'List of files: '
 		ls
 fi
