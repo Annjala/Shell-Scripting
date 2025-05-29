ecfe2a1d1e94a4e793754a705854afcc451013b9
Annjala Shaji <118911811+Annjala@users.noreply.github.com>
2025-05-29 14:04:46 +0530

    Update fileintegrity.sh

--- fileintegrity.sh
+++ fileintegrity.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#Test file integrity using hash
 
+#!/bin/bash
 echo 'To test integrity of a file in a directory at '$HOME ':'
 read -p "Enter the name of file: " file
+
 calculate_hash()
 {
 sha256sum $file | awk '{print $1}'
 }
+
 if [ ! -f $file ]; then
 echo "File does not exist!!"
 exit 1
 fi
+
 original_hash=$(calculate_hash $file)
 echo "Original hash: $original_hash"
 read -p "Make changes to the file and press Enter when done."
 new_hash=$(calculate_hash $file)
 echo "New hash: $new_hash"
+
 if [ "$original_hash" == "$new_hash" ]; then
 echo "File integrity is intact!"
 else
