83882b3c7e67c3ade06295be263360564982cf64
Annjala Shaji <118911811+Annjala@users.noreply.github.com>
2025-05-29 14:10:18 +0530

    Update userlist.sh

--- userlist.sh
+++ userlist.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#Display the count and list of users logged-in
 
+#!/bin/bash
 users=$(who | cut -d' ' -f1 | sort -u)
 count=$(echo "$users" | wc -l)
 echo "Number of users logged-in = $count "
